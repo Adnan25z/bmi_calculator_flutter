4cc666053693850726b3390cdb63099da0391e1b
Adnan <adnanzafar2001@gmail.com>
2025-07-19 22:14:20 +1000

    Refactored flutter widgets and created a skeleton UI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,53 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard(this.colour);
+  final Color colour;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0)
       ),
     );
   }
